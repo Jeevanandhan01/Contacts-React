b06772501a9ac0b052d0d791b2e5d106b5cf4408
Jeevanandhan <76152335+Jeevanandhan01@users.noreply.github.com>
2023-10-06 22:46:21 +0530

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+import apj from "./img/apj.jpg";
+import steve from "./img/steve.jpg";
+import bill from "./img/bill.jpg";
+
+const contacts = [
+  {
+    name: "Steve Jobs",
+    imgURL: steve,
+    phone: "+123 456 789",
+    email: "steve@jobs.com"
+  },
+  {
+    name: "Bill Gates",
+    imgURL: bill,
+    phone: "+987 654 321",
+    email: "bill@gates.com"
+  },
+  {
+    name: "Abdul kalam",
+    imgURL: apj,
+    phone: "+918 372 574",
+    email: "kalam@gmail.com"
+  }
+];
+export default contacts;
